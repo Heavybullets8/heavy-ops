095713181d8fe2e46ab1fcff4922249fe3cfe9ea
Heavybullets8 <heavybullets8@gmail.com>
2024-06-24 14:09:41 -0600

    run as root to run the db repair rq

--- kubernetes/apps/media/qbittorrent/cross-seed/config/config.js
+++ kubernetes/apps/media/qbittorrent/cross-seed/config/config.js
@@ -1,29 +1,28 @@
 module.exports = {
-    delay: 10,
-    torznab: [
-      `http://prowlarr.media.svc.cluster.local/19/api?apikey=$${process.env.PROWLARR_API_KEY}`,
-
-      `http://prowlarr.media.svc.cluster.local/31/api?apikey=$${process.env.PROWLARR_API_KEY}`,
-
-      `http://prowlarr.media.svc.cluster.local/20/api?apikey=$${process.env.PROWLARR_API_KEY}`,
-
-      `http://prowlarr.media.svc.cluster.local/28/api?apikey=$${process.env.PROWLARR_API_KEY}`,
-
-      `http://prowlarr.media.svc.cluster.local/21/api?apikey=$${process.env.PROWLARR_API_KEY}`
-    ],
-    qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local",
-    matchMode: "safe",
-    apiAuth: false,
-    outputDir: "/config",
-    torrentDir: "/qbittorrent/qBittorrent/BT_backup",
-    includeEpisodes: true,
-    includeNonVideos: false,
-    fuzzySizeThreshold: 0.02,
-    action: "inject",
-    duplicateCategories: true,
-    notificationWebhookUrl: `$${process.env.CROSS_SEED_WEBHOOK_URL}`,
-    rssCadence: "60min",
-    searchCadence: "7d",
+  action: "inject",
+  apiAuth: false,
+  delay: 10,
+  duplicateCategories: true,
+  flatLinking: true,
+  fuzzySizeThreshold: 0.02,
+  includeEpisodes: true,
+  includeNonVideos: false,
+  linkCategory: "cross-seed",
+  matchMode: "safe",
+  notificationWebhookUrl: `$${process.env.CROSS_SEED_WEBHOOK_URL}`,
+  outputDir: "/config",
+  qbittorrentUrl: "http://qbittorrent.media.svc.cluster.local",
+  rssCadence: "60min",
+  searchCadence: "7d",
+  torrentDir: "/qbittorrent/qBittorrent/BT_backup",
+  torznab: [
+    `http://prowlarr.media.svc.cluster.local/19/api?apikey=$${process.env.PROWLARR_API_KEY}`,
+    `http://prowlarr.media.svc.cluster.local/31/api?apikey=$${process.env.PROWLARR_API_KEY}`,
+    `http://prowlarr.media.svc.cluster.local/20/api?apikey=$${process.env.PROWLARR_API_KEY}`,
+    `http://prowlarr.media.svc.cluster.local/28/api?apikey=$${process.env.PROWLARR_API_KEY}`,
+    `http://prowlarr.media.svc.cluster.local/21/api?apikey=$${process.env.PROWLARR_API_KEY}`
+  ],
 };
+
 
 //# sourceMappingURL=config.template.docker.cjs.map
